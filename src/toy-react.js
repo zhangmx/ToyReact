ee8b69c95f9c137efa6428006f312c0bfefecef8
Zhang.mingxing <icediv@163.com>
2020-09-26 20:54:35 +0800

    fix range delete area

--- src/toy-react.js
+++ src/toy-react.js
@@ -69,8 +69,16 @@ export class Component {
     }
 
     rerender() {
-        this._range.deleteContents();
-        this[RENDER_TO_DOM](this._range)
+        let oldRange = this._range; 
+        
+        let range = document.createRange();
+        range.setStart(oldRange.startContainer,oldRange.startOffset);
+        range.setEnd(oldRange.startContainer,oldRange.startOffset);
+        this[RENDER_TO_DOM](range);
+
+        oldRange.setStart(range.endContainer, range.endOffset)
+        oldRange.deleteContents();
+        
     }
 
     setState(newState) {
